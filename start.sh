347a683e2453b1df671b2edf3dd41191de35808c
jhtshr <jhtshr@qq.com>
2020-03-16 13:40:04 +0800

    auto

--- start.sh
+++ start.sh
@@ -1,4 +1,10 @@
 #!/bin/bash
 echo "======================start make======================="
+rm Packages.bz2
 dpkg-scanpackages -m ./debs > Packages
 bzip2 Packages
+
+git add ./
+git commit -m auto
+git push
+
